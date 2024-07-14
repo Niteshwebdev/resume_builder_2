e8946e8a5d2b193f2c0d6f46e1fc9f2224e7fb3f
Niteshwebdev <worldbest178@gmail.com>
2024-07-14 13:46:12 -0700

    final code

---
+++ frontend/src/pages/download.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Download = ({ handleSave }) => {
+ 
+
+
+
+
+  return (
+    <div>
+      <button onClick={handleSave}>Download PDF</button>
+    </div>
+  );
+};
+
+export default Download;
